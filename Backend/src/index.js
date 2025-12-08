eececd12e2f0bfa28de2743c0a77b14fc2ca39ec
AdityaRaj-007 <iamadityaraj.2001@gmail.com>
2025-12-08 22:52:14 +0530

    removing

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -9,10 +9,6 @@ const PORT = 3000;
 
 app.use(express.json());
 
-//Password - cX2GOcK8vUxTjNlM
-//user - iamadityaraj2001_db_user
-//mongodb+srv://iamadityaraj2001_db_user:cX2GOcK8vUxTjNlM@cluster0.uug9vru.mongodb.net/?appName=Cluster0
-
 connectDB();
 
 app.use("/api", userRouter);
